cf88ddabe5818e853826dbb098f35b2b13b19e80
Xubris <Xubris@yandex.ru>
2026-03-09 10:28:31 -0700

    HomeWork8

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] weights = new int[]{90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
-        weights[0] = 90;
+        weights[0] = 70;
         int januaryWeight = weights[0];
         System.out.println(januaryWeight);
         System.out.println(weights[0]);
